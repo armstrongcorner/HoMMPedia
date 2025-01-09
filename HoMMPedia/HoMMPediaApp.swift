efce7992bad64945ee4a17d98ad20510dc1ba0c3
armstrongcorner <39514373+armstrongcorner@users.noreply.github.com>
2025-01-09 20:43:31 +1300

    Initial Commit

---
+++ HoMMPedia/HoMMPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoMMPediaApp.swift
+//  HoMMPedia
+//
+//  Created by Armstrong Liu on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HoMMPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
